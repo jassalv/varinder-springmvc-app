0070e944279f731de9d5a8c7e99ef8c532f4204b
Varinder jassal <varinderjassal@Varinders-MacBook-Pro.local>
2022-01-25 13:24:16 -0800

    just fixed the project structure

---
+++ src/main/java/com/spring/firstthymeleafapp/FirstThymeleafAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.firstthymeleafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstThymeleafAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstThymeleafAppApplication.class, args);
+	}
+
+}
