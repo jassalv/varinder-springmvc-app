fa59c99036ceadea753f7d600dd648d79dd8e3c3
Varinder jassal <varinderjassal@Varinders-MacBook-Pro.local>
2022-01-25 19:58:23 -0800

    minor changes

--- src/main/java/com/reactivestax/spring5mvc/Spring5mvcApplication.java
+++ src/main/java/com/reactivestax/spring5mvc/Spring5mvcApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5mvcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5mvcApplication.class, args);
 	}
